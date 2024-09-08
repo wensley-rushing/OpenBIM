b0ed8ee0c892dfb55d5bedf811bf885b0920af37
Wensley Rushing <179432206+wensley-rushing@users.noreply.github.com>
2024-09-07 20:43:20 -0700

    wr

--- src/openbim/csi/api/general_utilities.py
+++ src/openbim/csi/api/general_utilities.py
@@ -1,15 +1,8 @@
-'''
+"""
     MIT License
     
     Copyright (c) 2020 OpenSeesPro
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-    
+ 
     The above copyright notice and this permission notice shall be included in all
     copies or substantial portions of the Software.
     
@@ -35,7 +28,7 @@
         This script is a utility script to perform tasks supporting scraping data
         from ETABS as well as creating model in OpenSees and running analysis.
 
-'''
+"""
 
 import comtypes.client
 import sys
@@ -43,73 +36,87 @@ import time
 import pandas as pd
 import numpy as np
 
+
 def get_model_from_etabs():
     try:
         # attach to a running instance of ETABS
-        etabs = comtypes.client.GetActiveObject('CSI.ETABS.API.ETABSObject')
+        etabs = comtypes.client.GetActiveObject("CSI.ETABS.API.ETABSObject")
     except (OSError, comtypes.COMError):
         print("No running instance of the program found or failed to attach.")
         sys.exit(-1)
     return etabs.SapModel
 
-def set_load_cases_selected_for_display(loadCaseList, model = get_model_from_etabs()):
+
+def set_load_cases_selected_for_display(loadCaseList, model=get_model_from_etabs()):
     return model.DatabaseTables.SetLoadCasesSelectedForDisplay(loadCaseList)
 
-def set_load_combo_selected_for_display(loadComboList, model = get_model_from_etabs()):
+
+def set_load_combo_selected_for_display(loadComboList, model=get_model_from_etabs()):
     return model.DatabaseTables.SetLoadCombinationsSelectedForDisplay(loadComboList)
 
-def set_load_patterns_selected_for_display(loadPatternList, model = get_model_from_etabs()):
+
+def set_load_patterns_selected_for_display(
+    loadPatternList, model=get_model_from_etabs()
+):
     return model.DatabaseTables.SetLoadPatternsSelectedForDisplay(loadPatternList)
 
-def deselect_all_load_cases_and_combos_for_output(model = get_model_from_etabs()):
-    return [bool(~set_load_cases_selected_for_display('', model)[-1]), 
-            bool(~set_load_combo_selected_for_display('', model)[-1]),]
 
-def get_database_table_for_all_load_cases_and_combos(table_title, model=None):
-    # connect to ETABS model
-    if model is None:
-        model = get_model_from_etabs(True)
-    
+def deselect_all_load_cases_and_combos_for_output(model=get_model_from_etabs()):
+    return [
+        bool(~set_load_cases_selected_for_display("", model)[-1]),
+        bool(~set_load_combo_selected_for_display("", model)[-1]),
+    ]
+
+
+def get_database_table_for_all_load_cases_and_combos(table_title, model):
+
     # set units to kip, in (default for OpenSees)
     model.SetPresentUnits(3)
-    
+
     # get lists of all load cases and combinations
     try:
-        listOfLoadCases = [i for i in list(model.LoadCases.GetNameList()[-2]) if '~' not in i]
+        listOfLoadCases = [
+            i for i in list(model.LoadCases.GetNameList()[-2]) if "~" not in i
+        ]
     except:
         listOfLoadCases = []
-        
+
     try:
         listOfLoadCombos = list(model.RespCombo.GetNameList()[-2])
     except:
         listOfLoadCombos = []
-    
-    # select all laod cases and combinations for output
+
+    # select all load cases and combinations for output
     model.DatabaseTables.SetLoadCombinationsSelectedForDisplay(listOfLoadCombos)
     model.DatabaseTables.SetLoadCasesSelectedForDisplay(listOfLoadCases)
-    
+
     # get table from API
-    ret = model.DatabaseTables.GetTableForDisplayArray(table_title, '', '')
-    
+    ret = model.DatabaseTables.GetTableForDisplayArray(table_title, "", "")
+
     # develop DataFrame
     headers = list(ret[2])
     data = np.array(ret[4])
-    data = data.reshape(len(data)//len(headers), len(headers))
+    data = data.reshape(len(data) // len(headers), len(headers))
     df = pd.DataFrame(data)
-    
+
     cols_dict = dict(zip(df.columns.tolist(), headers))
-    df.rename(cols_dict, axis = 'columns', inplace = True)
-    
+    df.rename(cols_dict, axis="columns", inplace=True)
+
     return df.copy()
 
+
 def start_time():
-    print('Started at: ' + time.strftime('%a, %d %b %Y %H:%M:%S PST', time.localtime()))
+    print("Started at: " + time.strftime("%a, %d %b %Y %H:%M:%S PST", time.localtime()))
     return time.time()
 
+
 def end_time(start_time, final=True):
-    end_time    = time.time()
-    (mins, secs)= divmod(round(end_time - start_time, 0), 60)
-    print(f'\nTime Elapsed: {mins} minute(s) {secs} second(s).\n')
+    end_time = time.time()
+    (mins, secs) = divmod(round(end_time - start_time, 0), 60)
+    print(f"\nTime Elapsed: {mins} minute(s) {secs} second(s).\n")
     if final:
-        print('Finished at: ' + time.strftime('%a, %d %b %Y %H:%M:%S PST', time.localtime()))
+        print(
+            "Finished at: "
+            + time.strftime("%a, %d %b %Y %H:%M:%S PST", time.localtime())
+        )
     return
--- src/openbim/csi/api/opensees_postprocessor.py
+++ src/openbim/csi/api/opensees_postprocessor.py
@@ -1,4 +1,4 @@
-'''
+"""
     MIT License
     
     Copyright (c) 2020 OpenSeesPro
@@ -35,47 +35,50 @@
         This script is used to post-process the data generated through analyses
         in OpenSees.
 
-'''
+"""
 
 import os
-import numpy as np 
+import numpy as np
 import pandas as pd
 
-COLS = ['FX', 'FY', 'FZ', 'MX', 'MY', 'MZ']
-COL_DICT = {i:col for i, col in zip(range(6), COLS)}
+COLS = ["FX", "FY", "FZ", "MX", "MY", "MZ"]
+COL_DICT = {i: col for i, col in zip(range(6), COLS)}
+
 
 def post_process(initialOrTangent, dir_):
-    fpath = os.path.join(dir_, f'ele_frc_20279_{initialOrTangent}.out')
-    fpath2 = os.path.join(dir_, f'ele_def_20279_{initialOrTangent}.out')
-        
-    df = pd.DataFrame([s.split() for s in open(fpath, 'r').readlines()])
-    df['RY'] = pd.DataFrame([s.split() for s in open(fpath2, 'r').readlines()])[0]
+    fpath = os.path.join(dir_, f"ele_frc_20279_{initialOrTangent}.out")
+    fpath2 = os.path.join(dir_, f"ele_def_20279_{initialOrTangent}.out")
+
+    df = pd.DataFrame([s.split() for s in open(fpath, "r").readlines()])
+    df["RY"] = pd.DataFrame([s.split() for s in open(fpath2, "r").readlines()])[0]
     df = df.astype(float).rename(columns=COL_DICT)
-    
-    ax = df.plot(x='RY', y='MY', grid=True, figsize=(15,5))
+
+    ax = df.plot(x="RY", y="MY", grid=True, figsize=(15, 5))
     ax.set_axisbelow(True)
-    
-    df.to_excel(os.path.join(dir_, f'hinge_hyst-{initialOrTangent}.xlsx'))
+
+    df.to_excel(os.path.join(dir_, f"hinge_hyst-{initialOrTangent}.xlsx"))
     return df.copy()
 
+
 def base_shear(dir_, dict_of_rxn_nodes, initialOrTangent):
-    df_shear_x = pd.DataFrame(columns = [])
-    df_shear_x['t'] = np.arange(0,50.01,0.01)
+    df_shear_x = pd.DataFrame(columns=[])
+    df_shear_x["t"] = np.arange(0, 50.01, 0.01)
     df_shear_y = df_shear_x.copy()
-    
+
     for rxn_node in dict_of_rxn_nodes:
-        fpath = os.path.join(dir_, 'node_' + str(rxn_node) + '_rxn_' + initialOrTangent + '.out')
-        df = pd.DataFrame([s.split() for s in open(fpath, 'r').readlines()])
+        fpath = os.path.join(
+            dir_, "node_" + str(rxn_node) + "_rxn_" + initialOrTangent + ".out"
+        )
+        df = pd.DataFrame([s.split() for s in open(fpath, "r").readlines()])
         df = df.astype(float).rename(columns=COL_DICT)
-        
-        df_shear_x[f'X - {rxn_node}'] = df.FX
-        df_shear_y[f'Y - {rxn_node}'] = df.FY
-        
-    df_shear_x['Vx'] = df_shear_x.sum(axis = 1)
-    df_shear_y['Vy'] = df_shear_y.sum(axis = 1)
-    
-    df_shear_x.to_excel(os.path.join(dir_, f'base shear x-{initialOrTangent}.xlsx'))
-    df_shear_y.to_excel(os.path.join(dir_, f'base shear y-{initialOrTangent}.xlsx'))
-    
+
+        df_shear_x[f"X - {rxn_node}"] = df.FX
+        df_shear_y[f"Y - {rxn_node}"] = df.FY
+
+    df_shear_x["Vx"] = df_shear_x.sum(axis=1)
+    df_shear_y["Vy"] = df_shear_y.sum(axis=1)
+
+    df_shear_x.to_excel(os.path.join(dir_, f"base shear x-{initialOrTangent}.xlsx"))
+    df_shear_y.to_excel(os.path.join(dir_, f"base shear y-{initialOrTangent}.xlsx"))
+
     return
-        
