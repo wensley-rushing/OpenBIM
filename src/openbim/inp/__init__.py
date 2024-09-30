71668a9f2ee12e5e9bad25e4040a1e9e23612891
claudio perez <50180406+claudioperez@users.noreply.github.com>
2024-09-30 14:58:31 -0700

    cmp - add some models for Abaqus

--- src/openbim/inp/__init__.py
+++ src/openbim/inp/__init__.py
@@ -24,6 +24,8 @@ def create_model(obj, verbose=False):
     nu = 0.2
     mat = 1
     model.nDMaterial("ElasticIsotropic", mat, E, nu)
+    #                                       secTag  E     nu     h    rho
+    model.section("ElasticMembranePlateSection", 1, E, 0.25, 1.175, 1.27)
 
     #
     # Create Elements
@@ -35,6 +37,30 @@ def create_model(obj, verbose=False):
                 if len(nodes) == 8:
                     model.element("stdBrick", i, tuple(nodes), mat)
                     i += 1
+        elif block.type == "quad":
+            for nodes in block.data:
+                if len(nodes) == 4:
+                    model.element("ShellMITC4", i, tuple(nodes), mat, 1)
+                    i += 1
+                else:
+                    print("Quad with ", len(nodes), "nodes")
+
+        elif block.type == "line":
+            fsec = 1
+            model.section("FrameElastic", fsec, E=E, G=E*0.6, A=1, Iy=1, Iz=1, J=1)
+            model.geomTransf("Linear", 1, (0.0, 1.0, 0))
+            for nodes in block.data:
+                if len(nodes) == 2:
+                    model.element("PrismFrame", i, tuple(nodes), section=fsec, transform=1)
+                    i += 1
+
+        elif block.type == "triangle":
+            for nodes in block.data:
+                if len(nodes) == 3:
+                    model.element("ShellDKGT", i, tuple(nodes), mat, 1)
+                    i += 1
+        else:
+            print(block.type)
 
 
     return model
